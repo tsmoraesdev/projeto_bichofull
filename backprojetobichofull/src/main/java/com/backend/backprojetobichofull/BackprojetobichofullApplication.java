f35c8b3eac3989fd15d2ab27c09d77656dc53a23
Taty <2023008498@ifam.edu.br>
2026-02-19 16:29:10 -0400

    feat: estrutura inicial backend, frontend e docs

---
+++ backprojetobichofull/src/main/java/com/backend/backprojetobichofull/BackprojetobichofullApplication.java
@@ -0,0 +1,13 @@
+package com.backend.backprojetobichofull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackprojetobichofullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackprojetobichofullApplication.class, args);
+	}
+
+}
---
+++ backprojetobichofull/src/test/java/com/backend/backprojetobichofull/BackprojetobichofullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.backprojetobichofull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackprojetobichofullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
